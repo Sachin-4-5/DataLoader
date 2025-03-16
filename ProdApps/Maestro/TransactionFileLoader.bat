9e350f7a9af9a0e59f135c852c2f28695bff45dd
Sachin-4-5 <sachinkrana45@gmail.com>
2025-03-16 09:32:32 +0530

    initial commit

---
+++ ProdApps/Maestro/TransactionFileLoader.bat
@@ -0,0 +1,14 @@
+REM -----------------------
+REM Transaction File Loader
+REM -----------------------
+
+
+@echo off
+call "D:\Projects\Console Applications (Legacy)\ProdApps\Executable\DataLoader.exe"
+pause
+
+
+REM ---
+REM @echo off: Hides command outputs for a cleaner display.
+REM pause: Keeps the window open so you can see any messages or errors after execution.
+REM ---
